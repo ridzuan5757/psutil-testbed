662fdb7acdc3cd874241da2161b2482bffce9a66
ridzuan5757 <ridzuan5757@gmail.com>
2024-02-20 02:27:27 +0800

    unit test comple

--- hddmetrics_test.go
+++ hddmetrics_test.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"testing"
-
-	"github.com/shirou/gopsutil/v3/disk"
 )
 
 func TestGetHddMetrics(t *testing.T) {
@@ -32,47 +30,25 @@ func BenchmarkGetHddMetrics(b *testing.B) {
 
 func TestGetMountMetrics(t *testing.T) {
 
-	partitions := []disk.PartitionStat{
-		{
-			Device:     "foo",
-			Mountpoint: "/mnt/drive1",
-			Fstype:     "foo",
-			Opts:       []string{"foo"},
-		},
-		{
-			Device:     "foo",
-			Mountpoint: "/mnt/drive2",
-			Fstype:     "foo",
-			Opts:       []string{"foo"},
-		},
-	}
+	// Run the function
+	metrics, err := GetMountMetrics()
 
-	usage := map[string]*disk.UsageStat{
-		"mnt/drive1": {
-			Total:       100,
-			Used:        50,
-			Free:        50,
-			UsedPercent: 0.5,
-		},
-		"mnt/drive2": {
-			Total:       100,
-			Used:        50,
-			Free:        50,
-			UsedPercent: 0.5,
-		},
-	}
-
-	disk.Partitions = func(all bool) ([]disk.PartitionStat, error) {
-		return partitions, nil
-	}
-
-	disk.Usage = func(path string) *disk.UsageStat {
-		return usage[path], nil
-	}
-
-	mountMetrics, err := GetMountMetrics()
-
+	// Check if there was an error
 	if err != nil {
-		t.Errorf("Expected 2 mount metrics, got %d", len(mountMetrics))
+		t.Errorf("Error fetching HDD metrics: %v", err)
+	}
+
+	// Check if the metrics are reasonable (assuming we have at least one disk)
+	if len(metrics) == 0 {
+		t.Errorf("Unexpected total disk space: %d", len(metrics))
+	}
+}
+
+func BenchmarkGetMountMetrics(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := GetMountMetrics()
+		if err != nil {
+			b.Errorf("Error fetching HDD metrics: %v", err)
+		}
 	}
 }
