ac9ec175184cfda94c46748a3ccf573b18c89ec6
ridzuan5757 <ridzuan5757@gmail.com>
2024-02-19 08:31:44 +0800

    disk calc impl

--- main.go
+++ main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	//	"encoding/json"
 	"encoding/json"
 	"fmt"
 
 	"github.com/shirou/gopsutil/v3/disk"
 )
 
+type HddMetrics struct {
+	Total       uint64
+	Free        uint64
+	Used        uint64
+	UsedPercent float64
+}
+
 func main() {
-	diskUsage, _ := disk.Usage("/")
-	ppdp, _ := json.MarshalIndent(diskUsage, "", " ")
-	fmt.Println(string(ppdp))
+	partition, _ := disk.Partitions(false)
+	var pName []string
+	var hddMetrics HddMetrics
+
+	for _, p := range partition {
+		pName = append(pName, p.Mountpoint)
+	}
+
+	ppName, _ := json.MarshalIndent(pName, "", " ")
+	fmt.Println(string(ppName))
+
+	for _, name := range pName {
+		usage, err := disk.Usage(name)
+
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+
+		hddMetrics.Free += usage.Free
+		hddMetrics.Used += usage.Used
+		hddMetrics.Total += usage.Total
+
+	}
+
+	hddMetrics.UsedPercent = float64(hddMetrics.Used) / float64(hddMetrics.Total)
+
+	fmt.Println(hddMetrics)
+
 }
